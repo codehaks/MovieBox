57a3a6b02b1163e8c7daecb96a6ba9bde73068f3
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-11 02:57:57 +0430

    Init

---
+++ MyApp/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp
+{
+    public class IndexModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+
+            await LongRunningProccess();
+
+            return Page();
+        }
+
+        private async Task LongRunningProccess()
+        {
+            await Task.Delay(3000);
+
+            throw new Exception("Error!");
+        }
+    }
+}
---
+++ MyApp/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyApp
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+
+            });
+        }
+    }
+}
