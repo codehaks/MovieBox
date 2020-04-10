a4b6a3fab639135f9a2db705cc157d3a3b707136
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-11 03:11:14 +0430

    Fix

---
+++ MyApp/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApp.Models;
+
+namespace MyApp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public IActionResult OnPost()
+        {
+            using (var db = new LiteDB.LiteDatabase("movies.db"))
+            {
+                var movies = db.GetCollection<Movie>();
+                movies.Insert(Movie);
+
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
