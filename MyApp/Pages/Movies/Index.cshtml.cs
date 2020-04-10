135dc436c75b31aba6db2b7b2075d0e437aee119
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-11 03:06:07 +0430

    List

---
+++ MyApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApp.Models;
+
+namespace MyApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        public IList<Movie> MovieList { get; set; }
+        public void OnGet()
+        {
+            using (var db=new LiteDB.LiteDatabase("movies.db"))
+            {
+                var movies = db.GetCollection<Movie>();
+                MovieList = movies.FindAll().ToList();
+            }
+        }
+    }
+}
