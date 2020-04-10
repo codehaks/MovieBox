80e05d416ba9edf89bebb74848de47eb2e89f48f
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-11 03:27:13 +0430

    Edit

--- MyApp/Pages/Movies/Edit.cshtml.cs
+++ MyApp/Pages/Movies/Edit.cshtml.cs
@@ -8,17 +8,25 @@ using MyApp.Models;
 
 namespace MyApp.Pages.Movies
 {
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         [BindProperty]
         public Movie Movie { get; set; }
+
+        public void OnGet(int id)
+        {
+            using var db = new LiteDB.LiteDatabase("movies.db");
+            var movies = db.GetCollection<Movie>();
+
+            Movie = movies.FindById(id);
+        }
 
         public IActionResult OnPost()
         {
             using var db = new LiteDB.LiteDatabase("movies.db");
 
             var movies = db.GetCollection<Movie>();
-            movies.Insert(Movie);
+            movies.Update(Movie);
 
             return RedirectToPage("Index");
         }
