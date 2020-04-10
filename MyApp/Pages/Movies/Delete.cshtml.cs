979086bf1fa44c84cf51e1edfaa241af7b13cb9c
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-11 03:31:51 +0430

    Delete

---
+++ MyApp/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApp.Models;
+
+namespace MyApp.Pages.Movies
+{
+    public class DeleteModel : PageModel
+    {
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public void OnGet(int id)
+        {
+            using var db = new LiteDB.LiteDatabase("movies.db");
+            var movies = db.GetCollection<Movie>();
+
+            Movie = movies.FindById(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            using var db = new LiteDB.LiteDatabase("movies.db");
+
+            var movies = db.GetCollection<Movie>();
+            movies.Delete(Movie.Id);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
